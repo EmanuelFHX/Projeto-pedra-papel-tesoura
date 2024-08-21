aead499b35ceda5f70b74452d966c24a9e0f1fc8
EmanuelFHX <sagawar410@gmail.com>
2024-08-21 15:04:14 -0300

    adicionei ENUM

--- scripts.js
+++ scripts.js
@@ -6,13 +6,19 @@ const machineScore = document.querySelector("#machine-score")
 let humanScoreNumber = 0
 let machineScoreNumber = 0
 
+const GAME_OPTIONS = {
+    ROCK: 'rock',
+    PAPER: 'paper',
+    SCISSORS: 'scissors'
+}
+
 const playHuman = (humanChoice) => {
 
     playTheGame(humanChoice, playMachine())
 }
 
 const playMachine = () => {
-    const choices = ['rock', 'paper', 'scissors']
+    const choices = [GAME_OPTIONS.ROCK, GAME_OPTIONS.PAPER, GAME_OPTIONS.SCISSORS]
     const randomNumber = Math.floor(Math.random() * 3)
 
     return choices[randomNumber]
@@ -23,9 +29,10 @@ const playTheGame = (human, machine) => {
 
     if (human === machine) {
         result.innerHTML = "Empate"
-    } else if ((human === 'paper' && machine === 'rock') ||
-        (human === 'rock' && machine === 'scissors') ||
-        (human === 'scissors' && machine === 'paper')) 
+    } else if (
+        (human === GAME_OPTIONS.PAPER && machine === GAME_OPTIONS.ROCK) ||
+        (human === GAME_OPTIONS.ROCK && machine === GAME_OPTIONS.SCISSORS) ||
+        (human === GAME_OPTIONS.SCISSORS && machine === GAME_OPTIONS.PAPER)) 
         {
         humanScoreNumber++
         humanScore.innerHTML = humanScoreNumber
